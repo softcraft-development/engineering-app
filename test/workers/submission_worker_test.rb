54221df7a035ab06bc8396bb75061bc6fbcf12a2
Guyren Howe <ghowe@iparq.com>
2015-10-01 17:38:34 -0700

    Remove errant call to pry from test

--- test/workers/submission_worker_test.rb
+++ test/workers/submission_worker_test.rb
@@ -15,7 +15,6 @@ class SubmissionWorkerTest < Minitest::Test
     SubmissionWorker.new.perform({"g-recaptcha-response" => "12345", "email": "noreply@example.com"}, "1.1.1.1")
 
     assert 2, Mail::TestMailer.deliveries.length
-    require "pry"; binding.pry
   end
 
   def test_pass_recaptcha?
